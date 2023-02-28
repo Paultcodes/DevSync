07ea579a9285604eea0d75419f332e8fed1e8472
Kerry <kerryhokanson@gmail.com>
2023-02-27 20:54:20 -0600

    typedefs and resolvers for User

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,35 @@
-//!RESOLVERS GO HERE 
+const {User, Group, Message, Invite } = require ('../models')
+const {signToken} = require('../utils/auth')
+const {AuthenticationError} = require("apollo-server-express")
+
+const resolvers = {
+    Query: {
+        getSingleUser: async (parent, {user}) => {
+            return await User.findOne({
+                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+              });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({username: username, email: email, password: password});
+            const token = signToken(user)
+            return {user, token}
+        },
+
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if(!user){
+                throw new AuthenticationError("incorrect credentials")
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError("incorrect credentials")
+            }
+            const token = signToken(user);
+            return {token, user}
+        }
+    }
+}
+
+module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,28 @@
-//!Type defs go HERE!
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    ownedGroups: [Group]
+    groupsAsMember: [Group]
+    invites: [Invite]
+}
+
+type Group {
+    groupName: String
+    groupOwner: User
+    members: [User]
+    chatMessages: 
+}
+
+type message{
+    username: String
+    date: Date
+    message: String
+}
+`
+
+module.exports = typeDefs
