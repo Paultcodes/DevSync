13c8f63cd42786e7241dbe2665afd827c5b37fa8
Kerry <kerryhokanson@gmail.com>
2023-02-27 21:50:34 -0600

    typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,10 +18,29 @@ type Group {
     chatMessages: 
 }
 
-type message{
+type Message{
     username: String
     date: Date
     message: String
+}
+
+type Invite{
+    groupId: String
+    user: 
+}
+
+type Query{
+    getUser(userId: ID!): User
+    getMessages(messageId: ID!): Message
+    getGroup(GroupId: ID!): Group
+}
+
+type Mutation{
+    createUser(username: String, email: String!, password: String!): Auth
+    createMessage(messageText: String!, user: String, groupId: String!): Message
+    createGroup(groupName: String!, owner: User!): Group
+    createInvite(groupId: String!, groupName: String! userId: String!)
+    login(email: String!, password: String!) : Auth
 }
 `
 
