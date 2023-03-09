2f8f49e3ddbe7ef5e13b7b847173afd1756e639a
Paultcodes <paultayloremail1995@gmail.com>
2023-03-08 19:00:36 -0600

    test

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const secret = process.env.AUTH_SECRET;
 const expiration = '2h';
-
+//!
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
