b2c53abdad61aad9896faa18ba4125434de89be8
NuminousBlue <jdiehl2236@gmail.com>
2023-03-12 16:09:05 -0500

    remove some repetition from buttons component modulej

--- client/src/components/GroupSettings/GroupSettings.jsx
+++ client/src/components/GroupSettings/GroupSettings.jsx
@@ -1,26 +1,26 @@
-import { useState, useContext, useEffect } from 'react';
-import './GroupSettings.css';
-import { ButtonOne } from '../buttons/Buttons';
-import { useMutation } from '@apollo/client';
-import { useParams } from 'react-router-dom';
-import { ADD_TAGS_TO_GROUP } from '../../utils/mutations';
-import { GroupDataContext } from '../../pages/GroupPage/GroupPage';
+import { useState, useContext, useEffect } from "react";
+import "./GroupSettings.css";
+import { ButtonOne } from "../buttons/Buttons";
+import { useMutation } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { ADD_TAGS_TO_GROUP } from "../../utils/mutations";
+import { GroupDataContext } from "../../pages/GroupPage/GroupPage";
 
 const options = [
-  { label: 'JavaScript', value: 'javascript' },
-  { label: 'React', value: 'react' },
-  { label: 'Python', value: 'python' },
-  { label: 'Ecommerce', value: 'ecommerce' },
-  { label: 'MongoDB', value: 'mongodb' },
-  { label: 'MySQL', value: 'mysql' },
-  { label: 'Rust', value: 'rust' },
+  { label: "JavaScript", value: "javascript" },
+  { label: "React", value: "react" },
+  { label: "Python", value: "python" },
+  { label: "Ecommerce", value: "ecommerce" },
+  { label: "MongoDB", value: "mongodb" },
+  { label: "MySQL", value: "mysql" },
+  { label: "Rust", value: "rust" }
 ];
 
 const TagSelector = ({ refetch }) => {
   const tags = useContext(GroupDataContext);
   useEffect(() => {
     setSelectedTags([...tags.tags]);
-  }, []);
+  }, [tags]);
 
   const [addTagsToGroup] = useMutation(ADD_TAGS_TO_GROUP);
   const { groupId } = useParams();
@@ -36,19 +36,19 @@ const TagSelector = ({ refetch }) => {
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     console.log(selectedTags);
     event.preventDefault();
 
     try {
-      const { data } = addTagsToGroup({
+      await addTagsToGroup({
         variables: {
           groupId,
-          tags: selectedTags,
-        },
+          tags: selectedTags
+        }
       });
       refetch();
-      setShowResponse(true)
+      setShowResponse(true);
     } catch (err) {
       console.log(err);
     }
--- client/src/components/buttons/Buttons.jsx
+++ client/src/components/buttons/Buttons.jsx
@@ -1,17 +1,21 @@
+const baseBtnStyles = {
+  textAlign: "center",
+  textDecoration: "none",
+  display: "inline-block",
+  cursor: "pointer"
+};
+
 export const ButtonOne = (props) => {
   return (
     <button
       style={{
+        ...baseBtnStyles,
         backgroundColor: "#00008B",
         border: "none",
         color: "white",
         padding: "12px 24px",
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
         fontSize: "16px",
         borderRadius: "4px",
-        cursor: "pointer",
         boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
         transition: "background-color 0.3s ease"
       }}
@@ -27,17 +31,14 @@ export const ButtonTwo = (props) => {
   return (
     <button
       style={{
+        ...baseBtnStyles,
         backgroundColor: "#fff",
         border: "2px solid #2D2D2D",
         color: "#2D2D2D",
         padding: "12px 24px",
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
         fontSize: "16px",
         fontWeight: "bold",
         borderRadius: "4px",
-        cursor: "pointer",
         boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
         transition:
           "background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease"
@@ -54,16 +55,13 @@ export const ButtonThree = (props) => {
     <button
       name={props.name}
       style={{
+        ...baseBtnStyles,
         backgroundColor: "#D63447",
         border: "none",
         color: "#fff",
         padding: "14px 24px",
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
         fontSize: "18px",
         borderRadius: "40px",
-        cursor: "pointer",
         boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.25)",
         transition: "background-color 0.3s ease, transform 0.3s ease"
       }}
@@ -78,16 +76,13 @@ export const ButtonFour = (props) => {
   return (
     <button
       style={{
+        ...baseBtnStyles,
         backgroundColor: "#00008B",
         border: "none",
         color: "#fff",
         padding: "9px 15px",
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
         fontSize: "16px",
         borderRadius: "50px",
-        cursor: "pointer",
         boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
         transition: "background-color 0.3s ease, transform 0.3s ease"
       }}
